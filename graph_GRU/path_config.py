23cacac355a5e660fca5c5a83225be9c02977535
KyungMinseo1 <kminxi0217@g.skku.edu>
2025-12-28 19:17:58 +0900

    graph_GRU first commit

---
+++ graph_GRU/path_config.py
@@ -0,0 +1,9 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.join(BASE_DIR, '..')
+
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+RAW_DATA_DIR = os.path.join(DATA_DIR, 'Raw Data')
+
+MODEL_DIR = os.path.join(ROOT_DIR, 'model')
